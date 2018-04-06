49bde6eb391aa1ddfb78213ace36de8fb553f316
Matthew Balaam <mattybalaam@gmail.com>
2018-04-06 14:59:24 +0100

    Switch position of technologies to further up.

--- my-app/src/js/CVApp.jsx
+++ my-app/src/js/CVApp.jsx
@@ -45,9 +45,9 @@ class App extends Component {
           <div className="cube__face-2">
             <article className="cv__grid"> 
               <Contact {...this.state.content.basics} />
+              <Technologies technologies={this.state.content.skills[0].keywords} /> 
               <Experience work={this.state.content.work} />
               <Education education={this.state.content.education} />
-              <Technologies technologies={this.state.content.skills[0].keywords} /> 
             </article>
           </div>
         </div>
