2b22517d558cc8ecab3afd22f9fef0407eabfa1d
Matthew Balaam <mattybalaam@gmail.com>
2018-04-06 15:18:47 +0100

    Change directory structure.

--- README.md
+++
@@ -1,2 +0,0 @@
-# matthewbalaam-co-uk
-
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import CVApp from './js/CVApp';
 import registerServiceWorker from './registerServiceWorker';
 
+
+console.log('make sure the PDF is added');
+
 ReactDOM.render(<CVApp />, document.getElementById('root'));
 registerServiceWorker();
