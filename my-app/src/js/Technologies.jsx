a942d6396732ee0fcf5971af38c226a095ca4a99
Matthew Balaam <mattybalaam@gmail.com>
2018-04-06 14:57:06 +0100

    Split text from bullet points to allow different colour styling

--- my-app/src/js/Technologies.jsx
+++ my-app/src/js/Technologies.jsx
@@ -4,7 +4,11 @@ const Technologies = props => (
     <article className="technology grid">
       <h2 className="technology__header section-header">Technologies</h2>
       <ul className="technology__list section__content">
-        {props.technologies.map((item, i) => <li key={i} className="technology__item">{item}</li>)}
+        {props.technologies.map((item, i) => {
+          return (<li key={i} className="technology__item">
+            <span className="technology__item-text">{item}</span>
+          </li>)
+          })}
       </ul>
     </article>
   );
