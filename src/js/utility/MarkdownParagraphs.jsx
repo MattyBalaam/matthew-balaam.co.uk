fc2b47244a993f1284bfb278c2201f7bbcb4daad
Matthew Balaam <mattybalaam@gmail.com>
2018-04-12 21:37:33 +0100

    Rewrite MarkdownParagraphs to add BEM style classes

--- src/js/utility/MarkdownParagraphs.jsx
+++ src/js/utility/MarkdownParagraphs.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import React, {createElement} from 'react';
+import ReactMarkdown, {renderers} from 'react-markdown';
+import mapObj from './MapObj';
 
-//toDo: needs to check what kind of element is created before adding className
-const root = props => (
-    <>
-      {props.children.map(child => {
-          return {...child, 
-            props: {
-              ...child.props, 
-              className: 'paragraphs__p',
-            }
-          }
-        })
-      }
-    </>
+const BASECLS = 'mkd mkd--';
+
+const addBemToMarkdown = type => props => createElement(type, {...props, className: `${BASECLS}${type}`}, props.children);
+
+const addBemToFunc = oldFunc => props => {
+  const component = oldFunc(props); 
+  return {
+    ...component, 
+    props: {
+      ...component.props,
+      className: `${BASECLS}${component.type}`,
+    }
+  };
+}
+
+const bemRenderers = mapObj(renderers, ([k, v]) => {
+    if (typeof v === 'function') {
+      v = addBemToFunc(v)
+    } else {
+      v = addBemToMarkdown(v);
+    }
+    return {[k]: v};
+  }
+);
+
+
+const MarkdownParagraphs = props => {
+  let source = props.source;
+  if (typeof source.reduce === 'function') {
+    source = source.reduce((str, val) => str + '\n\n' + val );
+  } 
+  return (
+      <ReactMarkdown source={source} renderers={bemRenderers}/>
   )
-
-const MarkdownParagraphs = props => (
-    <ReactMarkdown source={props.source} renderers={{root}}/>
-)
+}
 
 export default MarkdownParagraphs;
