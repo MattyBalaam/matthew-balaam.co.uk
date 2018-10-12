53c55079acff55fcea8966a3d3a3ba9d78228c90
Matthew Balaam <mattybalaam@gmail.com>
2018-10-12 12:48:00 +0100

    Update dependencies. CRA2 etc.

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,16 @@
-const rewireEslint = require('react-app-rewire-eslint');
+const rewireEslint = require("react-app-rewire-eslint");
+const rewirePostCSS = require("react-app-rewire-postcss");
 module.exports = function override(config, env) {
- config = rewireEslint(config, env);
- return config;
-}
+  config = rewireEslint(config, env);
+  console.log(config);
+  config = rewirePostCSS(config, {
+    plugins: loader => [
+      require("postcss-import")(),
+      require("postcss-nested")(),
+      require("postcss-custom-media")(),
+      require("postcss-flexbugs-fixes")()
+    ]
+  });
+  console.log(config);
+  return config;
+};
