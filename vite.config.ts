d247a82be03d93d70dd7b41b3d86b965ba20f2dc
Matthew Balaam <mattybalaam@gmail.com>
2025-11-18 18:53:48 +0000

    netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
