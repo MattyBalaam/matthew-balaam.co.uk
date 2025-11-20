7dc2e07a913a34d656d0ef3dab0872bf9d5df190
Matthew Balaam <mattybalaam@gmail.com>
2025-11-20 15:54:06 +0000

    small conmit

--- app/routes/music.tsx
+++ app/routes/music.tsx
@@ -4,37 +4,40 @@ import { href, Link, type MetaDescriptor } from "react-router";
 import type { Route } from "./+types/music";
 import * as styles from "./music.css";
 
-export function loader() {
-  return {
-    posts: [
-      {
-        title: "Warm up set at Renae Manchester",
-        url: "https://plyr.fm/track/70",
-        description:
-          "I played 5-8pm on Saturday 9th of November 2025, here is the recording",
-      },
-      {
-        title: "Special Treatment on Slack City Radio",
-        url: "https://www.totallyradio.com/shows/special-treatment",
-        description:
-          "Special Treatment radio show every 4 weeks on www.slackcityradio.com",
-      },
-      {
-        title: "The Flex - italo euro party",
-        url: "https://www.instagram.com/theflexdisko/",
-        description: "The Flex Disko Instagram - updates and more",
-      },
-      {
-        title: "inter~mission on 1BTN radio",
-        url: "https://soundcloud.com/glocknspiel/reposts",
-        description: "inter~mission shows on www.1btn.fm",
-      },
-    ],
-  };
-}
-
 export function meta() {
   return [{ title: "Matthew Balaam - Music" }] satisfies Array<MetaDescriptor>;
+}
+
+// Placeholder until a time a DB or CMS is used
+const posts = [
+  {
+    title: "Warm up set at Renae Manchester",
+    url: "https://plyr.fm/track/70",
+    description:
+      "I played 5-8pm on Saturday 9th of November 2025, here is the recording",
+  },
+  {
+    title: "Special Treatment on Slack City Radio",
+    url: "https://www.totallyradio.com/shows/special-treatment",
+    description:
+      "Special Treatment radio show every 4 weeks on www.slackcityradio.com",
+  },
+  {
+    title: "The Flex - italo euro party",
+    url: "https://www.instagram.com/theflexdisko/",
+    description: "The Flex Disko Instagram - updates and more",
+  },
+  {
+    title: "inter~mission on 1BTN radio",
+    url: "https://soundcloud.com/glocknspiel/reposts",
+    description: "inter~mission shows on www.1btn.fm",
+  },
+];
+
+export function loader() {
+  return {
+    posts,
+  };
 }
 
 export default function Music({ loaderData: { posts } }: Route.ComponentProps) {
